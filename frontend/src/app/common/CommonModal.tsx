db01b168cd1e749764540dbcee1142fe1f0d78ff
temevh <temehama@gmail.com>
2026-02-28 09:27:47 +0200

    masonry for topic adding

--- frontend/src/app/common/CommonModal.tsx
+++ frontend/src/app/common/CommonModal.tsx
@@ -1,13 +1,13 @@
-import { Box, Button, Modal, Typography } from "@mui/material";
+import { Box, Modal } from "@mui/material";
+import { colors } from "../styles";
 
 const style = {
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+  width: 600,
+  bgcolor: colors.background.paper,
   boxShadow: 24,
   p: 4,
 };
--- frontend/src/app/components/TopicPicker.tsx
+++ frontend/src/app/components/TopicPicker.tsx
@@ -1,8 +1,29 @@
+import { TopicBadge } from "./TopicBadge";
+
 const TopicPicker = ({ topics }) => {
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "12px",
+        padding: "1rem",
+        justifyContent: "center",
+      }}
+    >
       {topics.map((topic) => (
-        <p key={topic.id}>{topic.name}</p>
+        <div
+          key={topic.name}
+          style={{
+            height: 30,
+            lineHeight: 30,
+
+            margin: "0 4px 4px 0",
+            textAlign: "center",
+          }}
+        >
+          <TopicBadge topic={topic.name} />
+        </div>
       ))}
     </div>
   );
